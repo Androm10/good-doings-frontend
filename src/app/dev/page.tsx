83451dd32c965fdc9617a419563d980ff4a413ce
androm <turin636@gmail.com>
2023-07-10 10:10:06 +0300

    added dev, profile pages

---
+++ src/app/dev/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const DevPage: FC = () => {
+  return (
+    <div>
+      <h1>DEV</h1>
+    </div>
+  );
+};
+DevPage.displayName = "DevPage";
+
+export default DevPage;
--- src/core/entities/user.entity.ts
+++ src/core/entities/user.entity.ts
@@ -2,5 +2,5 @@ export type UserEntity = {
   id: number;
   login: string;
   username: string;
-  password: string;
+  publicId: string;
 };
